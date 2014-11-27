a53b33a4022efaf62a2669664875bcffb83c0f79
XUZMA <xuzmnsccsz@yeah.net>
2014-11-27 21:15:08 +0800

    Simple examples from www.fpcomplete.com

--- TCOFP/Code/fpcomplete001.hs
+++ TCOFP/Code/fpcomplete001.hs
@@ -1,7 +1,14 @@
-import System.Info
+-- runhugs fpcomplete001.hs 
 
-main = do
-    print os
-    print arch
-    print compilerName
-    print compilerVersion
+-- main = do
+--    putStrLn "Hello, world!"
+--    putStrLn "https://www.fpcomplete.com/school/to-infinity-and-beyond/pick-of-the-week/Simple%20examples"
+
+import Data.Time
+main = getCurrentTime >>= print
+
+-- main = do
+--    print os
+--    print arch
+--    print compilerName
+--    print compilerVersion
