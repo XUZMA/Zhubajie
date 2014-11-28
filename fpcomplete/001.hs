c8bff7f6fbcfe644e518d1e1b582aa36267c1083
XUZMA <xuzmnsccsz@yeah.net>
2014-11-28 23:23:55 +0800

    Simple examples from www.fpcomplete.com

---
+++ fpcomplete/001.hs
@@ -0,0 +1,7 @@
+-- runhugs 001.hs 
+
+import Data.Time
+main = do
+    putStrLn "Hello, world!"
+    getCurrentTime >>= print
+    putStrLn "https://www.fpcomplete.com/school/to-infinity-and-beyond/pick-of-the-week/Simple examples"
