77004ee4e1cb8b7706df481975b82e87672f9632
XUZMA <xuzmnsccsz@yeah.net>
2014-12-10 23:08:30 +0800

    putStrLn

--- fpcomplete/001.hs
+++ fpcomplete/001.hs
@@ -5,3 +5,4 @@ main = do
     putStrLn "Hello, world!"
     getCurrentTime >>= print
     putStrLn "https://www.fpcomplete.com/school/to-infinity-and-beyond/pick-of-the-week/Simple examples"
+    putStrLn "ok"
